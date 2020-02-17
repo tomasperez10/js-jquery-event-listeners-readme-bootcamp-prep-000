0edf187b4ef1b3c710065470c12fa89ce96990f9
Tomas Perez <tomasitopg6@gmail.com>
2020-02-17 23:00:00 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
 //define functions here
 
+function getIt(){
+  $("p").on("click", function(){
+    window.alert("Hey!");
+    });
+  }
+  
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $("input").on("keydown", function(e){
+    if (e.which === 71){
+      window.alert("G was pressed.");
+    }
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    window.alert("Your form is going to be submitted now.");
+  });
+}
+
+getIt();
+frameIt();
+pressIt();
+submitIt();
+
 $(document).ready(function(){
 
 // call functions here
 
+
 });
+
